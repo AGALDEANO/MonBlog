627793b6f6fcf015ede13477302a35c095d91e71
AGALDEANO <alexandre.galdeano@etu.univ-lyon1.fr>
2014-02-25 17:09:05 +0100

    Ajout du gabarit

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8',
-            'root', '');
-    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-            . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-            . ' order by BIL_ID desc');
+require 'Modele.php';
+$billets = getBillets();
 require 'vueAccueil.php';
             
