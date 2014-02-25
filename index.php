86aa98a8048b40cff14caee7915fcd61616060a9
AGALDEANO <alexandre.galdeano@etu.univ-lyon1.fr>
2014-02-25 16:37:46 +0100

    Base de travail

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div id="global">
-            <header>
+            <header> 
                 <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
                 <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
             </header>
