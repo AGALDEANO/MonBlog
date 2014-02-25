53672c499e6c989e4c2f3a73ea4e941628e220a1
AGALDEANO <alexandre.galdeano@etu.univ-lyon1.fr>
2014-02-25 17:20:15 +0100

    Gestion d'erreur

--- Modele.php
+++ Modele.php
@@ -1,10 +1,13 @@
 <?php
 function getBillets() {
-    $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8',
-            'root', '');
+    $bdd = getBDD();
     $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
             . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
             . ' order by BIL_ID desc');
     return $billets;
-    
+}
+function getBDD() {
+    $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8',
+            'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $bdd;
 }
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require 'Modele.php';
-$billets = getBillets();
-require 'vueAccueil.php';
-            
+try {
+  $billets = getBillets();
+  require 'vueAccueil.php';
+}
+catch (Exception $e) {
+  $msgErreur = $e->getMessage();
+  require 'vueErreur.php';
+}
